2b176f51006c05541a86ab8c15ca84e7bc815b0d
vaanloal <79015920+vaanloal@users.noreply.github.com>
2022-12-19 15:09:04 +0000

    formating

--- Destilação/GetAlpha.m
+++ Destilação/GetAlpha.m
@@ -1,9 +1,9 @@
-function [alpha_mean, alpha] = GetAlpha(Ant,P,visualize)
+function [alpha_mean, alpha] = GetAlpha(Ant, P, visualize)
 
-    P_vap_Col = @(TdegC) 10.^(Ant.Col(1) - Ant.Col(2) ./ (TdegC + Ant.Col(3)));
-    P_vap_Xyl = @(TdegC) 10.^(Ant.Xil(1) - Ant.Xil(2) ./ (TdegC + Ant.Xil(3)));
-    T_vap_Col = @(P) (Ant.Col(2) + log10(P)*Ant.Col(3) - (Ant.Col(1)*Ant.Col(3)))/(Ant.Col(1) - log10(P));
-    T_vap_Xyl = @(P) (Ant.Xil(2) + log10(P)*Ant.Xil(3) - (Ant.Xil(1)*Ant.Xil(3))) / (Ant.Xil(1) - log10(P));
+    P_vap_Col = @(TdegC) 10 .^ (Ant.Col(1) - Ant.Col(2) ./ (TdegC + Ant.Col(3)));
+    P_vap_Xyl = @(TdegC) 10 .^ (Ant.Xil(1) - Ant.Xil(2) ./ (TdegC + Ant.Xil(3)));
+    T_vap_Col = @(P) (Ant.Col(2) + log10(P) * Ant.Col(3) - (Ant.Col(1) * Ant.Col(3))) / (Ant.Col(1) - log10(P));
+    T_vap_Xyl = @(P) (Ant.Xil(2) + log10(P) * Ant.Xil(3) - (Ant.Xil(1) * Ant.Xil(3))) / (Ant.Xil(1) - log10(P));
     alpha_LK_HK = @(T) P_vap_Xyl(T) ./ P_vap_Col(T);
 
     x = linspace(0, 1, 100); % fração do LK
@@ -13,13 +13,13 @@ function [alpha_mean, alpha] = GetAlpha(Ant,P,visualize)
     alpha_mean = mean(alpha);
 
     if visualize == 1
-    hfig = figure(Color = "w");
-    plot(T_mix, log10(alpha), 'LineWidth', 1.5)
-    xlabel("$T_{\rm mix}\; \rm[^oC]$", Interpreter = "latex")
-    ylabel("$\log_{10}(\alpha_{\rm LK/HK})$", Interpreter = "latex")
-    ax = gca;
-    SetFigureDefaults2(hfig, 0.5, 15, 12, ax)
-    axis tight
+        hfig = figure(Color = "w");
+        plot(T_mix, log10(alpha), 'LineWidth', 1.5)
+        xlabel("$T_{\rm mix}\; \rm[^oC]$", Interpreter = "latex")
+        ylabel("$\log_{10}(\alpha_{\rm LK/HK})$", Interpreter = "latex")
+        ax = gca;
+        SetFigureDefaults2(hfig, 0.5, 15, 12, ax)
+        axis tight
     else
     end
 
--- Destilação/LMTD.m
+++ Destilação/LMTD.m
@@ -1,8 +1,6 @@
 function [deltaT] = LMTD(Thin, Thout, Tcin, Tcout)
-delta1 = Thin - Tcout;
-delta2 = Thout - Tcin;
+    delta1 = Thin - Tcout;
+    delta2 = Thout - Tcin;
 
-
-deltaT = (delta1 - delta2) / log(delta1 / delta2);
+    deltaT = (delta1 - delta2) / log(delta1 / delta2);
 end
-
